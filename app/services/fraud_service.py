6d133fa0d2a32993d82aa277b9dadddb78293f8a
Najari Umar Jibril <najariuj@gmail.com>
2026-06-07 01:59:23 +0100

    fixed fraud endpoint

--- app/services/fraud_service.py
+++ app/services/fraud_service.py
@@ -14,15 +14,7 @@ def predict_fraud(amount, device_change, location_change, transactions_today):
 
     prediction = model.predict(data)[0]
 
-    risk_score = float(model.predict_proba(data)[0][1] * 100)
-
-    if prediction == 1:
-        status = "Suspicious"
-    else:
-        status = "Normal"
-
     return {
         "fraud_risk": int(prediction),
-        "risk_score": round(risk_score, 2),
-        "status": status
+        "status": "Suspicious" if prediction == 1 else "Safe"
     }
